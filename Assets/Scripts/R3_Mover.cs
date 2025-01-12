f611a09c581c1ee47103c8c456dc1591e6ad650a
Bokjihee <bokjihee@naver.com>
2025-01-12 19:18:41 +0900

    [feat] 양 애니메이션 추가 및 weapon 이동방법 변경

--- Assets/Scripts/R3_Mover.cs
+++ Assets/Scripts/R3_Mover.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [Header("Settings")]
-    public float moveSpeed; // 아래로 떨어지는 속도
-    public float rightMoveSpeed = 5f; // 오른쪽 이동 속도
+    public float moveSpeed;
+    private bool isInsideBox = false; 
+    private bool isMovingRight = false;
+    private Animator SheepAnimator;  // 양 애니메이션
 
-    private bool isInsideBox = false; // 상자 내부에 있는지 여부
-    private bool isMovingRight = false; // 오른쪽으로 이동 중인지 여부
+    void Start()
+    {
+        SheepAnimator = Object.FindAnyObjectByType<R3_Sheep>()?.GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -17,24 +21,28 @@ public class Mover : MonoBehaviour
         }
         else
         {
-            transform.position += Vector3.right * rightMoveSpeed * Time.deltaTime;
+            transform.position += Vector3.right * 4 * Time.deltaTime;
+            transform.position += Vector3.up * 9 * Time.deltaTime;
         }
 
         if (isInsideBox && Input.GetKeyDown(KeyCode.Space))
         {
+            Debug.Log("스페이스 눌림!");
             isMovingRight = true;
+            TriggerSheepAnimation();
+
+            transform.position = new Vector2(-1.645f, -1.507f); //위치 이동
         }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        // "Box" 태그를 가진 상자와 충돌했을 때
         if (other.CompareTag("Box"))
         {
+            Debug.Log("충돌함!");
             isInsideBox = true;
         }
 
-        // "Wolf" 태그를 가진 객체와 충돌했을 때
         if (other.CompareTag("Wolf"))
         {
             Destroy(gameObject);
@@ -47,6 +55,32 @@ public class Mover : MonoBehaviour
         if (other.CompareTag("Box"))
         {
             isInsideBox = false;
+        }
+    }
+
+    //양 애니메이션
+    void TriggerSheepAnimation()
+    {
+        string myTag = gameObject.tag;
+
+        if (myTag == "Rock")
+        {
+            Debug.Log("돌 양 애니메이션 실행!");
+            SheepAnimator.SetTrigger("RockSheep");
+        }
+        else if (myTag == "Leaf")
+        {
+            Debug.Log("나뭇잎 양 애니메이션 실행!");
+            SheepAnimator.SetTrigger("LeafSheep");
+        }
+        else if (myTag == "Tree")
+        {
+            Debug.Log("나무 양 애니메이션 실행!");
+            SheepAnimator.SetTrigger("TreeSheep");
+        }
+        else
+        {
+            Debug.LogWarning("무기의 태그가 정의되지 않았습니다!");
         }
     }
 }
