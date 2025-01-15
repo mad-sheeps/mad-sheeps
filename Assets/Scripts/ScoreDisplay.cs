b63e37c6bb9f11ce17b14757782e5ec083e2d38c
yeonwoo <yeonu0681658@gmail.com>
2025-01-15 16:23:39 +0900

    feat- 스코어 화면 버튼 연결

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -6,6 +6,7 @@ public class ScoreDisplay : MonoBehaviour
     public TextMeshProUGUI R1_Score_Text;
     public TextMeshProUGUI R2_Score_Text;
     public TextMeshProUGUI R3_Score_Text;
+    public TextMeshProUGUI Toral_Score_Text;
 
     void Start()
     {
@@ -13,10 +14,11 @@ public class ScoreDisplay : MonoBehaviour
         int r1Score = PlayerPrefs.GetInt("Round1", 0); // 기본값 0
         int r2Score = PlayerPrefs.GetInt("Round2", 0); // 기본값 0
         int r3Score = PlayerPrefs.GetInt("Round3", 0); // 기본값 0
-
+        int total = r1Score+r2Score+r3Score;
         // 점수를 UI에 표시
         R1_Score_Text.text = $"{r1Score}";
         R2_Score_Text.text = $"{r2Score}";
         R3_Score_Text.text = $"{r3Score}";
+        Toral_Score_Text.text = $"{total}";
     }
 }
