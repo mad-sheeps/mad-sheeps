68bc0f1ddefbe4f0ed807395edd07194067f3c39
Bokjihee <bokjihee@naver.com>
2025-01-15 18:42:24 +0900

    [feat] add bestScore

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -7,6 +7,7 @@ public class ScoreDisplay : MonoBehaviour
     public TextMeshProUGUI R2_Score_Text;
     public TextMeshProUGUI R3_Score_Text;
     public TextMeshProUGUI Toral_Score_Text;
+    public TextMeshProUGUI Best_Score_Text;
 
     void Start()
     {
@@ -15,10 +16,17 @@ public class ScoreDisplay : MonoBehaviour
         int r2Score = PlayerPrefs.GetInt("Round2", 0); // 기본값 0
         int r3Score = PlayerPrefs.GetInt("Round3", 0); // 기본값 0
         int total = r1Score+r2Score+r3Score;
+        int bestScore = PlayerPrefs.GetInt("BestScore", 0); // 기본값 0
+        //최고점수 갱신
+        if(bestScore < total){
+            PlayerPrefs.SetInt("BestScore", total);
+            PlayerPrefs.Save();
+        }
         // 점수를 UI에 표시
         R1_Score_Text.text = $"{r1Score}";
         R2_Score_Text.text = $"{r2Score}";
         R3_Score_Text.text = $"{r3Score}";
         Toral_Score_Text.text = $"{total}";
+        Best_Score_Text.text = $"{bestScore}";
     }
 }
